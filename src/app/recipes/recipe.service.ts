0b683605f3e3ebf6b6c2393aa01cc53ee80139c7
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-01 20:27:52 +0200

    Ingredients are now available on the detail page.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,15 @@
 import {Recipe,} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
-export class RecipeService{
+export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [new Ingredient('Meat', 1),new Ingredient('French Fries', 20)]),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',[new Ingredient('buns', 2),new Ingredient('Meat', 1)])
   ];
-getRecipes(){
-  return this.recipes.slice();
-}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }

7fa77b7ee0cc88909749ad6f0db36e7043390786
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-05 06:31:21 +0200

    Added recipes Home component as child route to recipes.

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,10 +2,11 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {RecipesComponent} from "./recipes/recipes.component";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {RecipeHomeComponent} from "./recipes/recipe-home/recipe-home.component";
 
 const appRoutes:Routes = [
  {path:'',redirectTo:'recipes',pathMatch:'full'},
-  {path:'recipes',component:RecipesComponent},
+  {path:'recipes',component:RecipesComponent,children:[{path:'',component:RecipeHomeComponent}]},
   {path:'shoppinglist',component:ShoppingListComponent},
 
 ];

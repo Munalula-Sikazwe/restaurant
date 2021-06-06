bc2d8c2b0277e7260151f0a62eb251b796403c10
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-06 02:40:38 +0200

    Created the recipe edit component

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,17 +4,25 @@ import {RecipesComponent} from "./recipes/recipes.component";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 import {RecipeHomeComponent} from "./recipes/recipe-home/recipe-home.component";
 import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
+import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
 
-const appRoutes:Routes = [
- {path:'',redirectTo:'recipes',pathMatch:'full'},
-  {path:'recipes',component:RecipesComponent,children:[{path:'',component:RecipeHomeComponent},{path:':id',component:RecipeDetailComponent}]},
-  {path:'shoppinglist',component:ShoppingListComponent},
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'recipes', pathMatch: 'full'},
+  {
+    path: 'recipes', component: RecipesComponent,
+    children: [{path: '', component: RecipeHomeComponent},
+      {path: 'new', component: RecipeEditComponent},
+      {path: ':id', component: RecipeDetailComponent},]
+  },
+
+  {path: 'shoppinglist', component: ShoppingListComponent},
 
 ];
+
 @NgModule({
-  imports:[RouterModule.forRoot(appRoutes)],
-  exports:[RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule{
+export class AppRoutingModule {
 
 }
